868c78d4e62a9ba610328053796d8d4c24357801
Debarghya Datta <ujan.dattaa@gmail.com>
2022-08-17 00:11:44 +0530

    fix

--- Arrays/countfrequency.java
+++ Arrays/countfrequency.java
@@ -1,4 +1,5 @@
 package Arrays;
+
 import java.util.*;
 
 public class countfrequency {
@@ -25,12 +26,12 @@ public class countfrequency {
     }
 }
 
-//import java.util.*;
-//
-//public class tuf {
-//
+// import java.util.*;
+
+// public class countfrequency {
+
 //    public static void main(String args[]){
-//
+
 //        int arr[] ={10,5,10,15,10,5};
 //        int n = arr.length;
 //        countFreq(arr, n);
@@ -38,13 +39,13 @@ public class countfrequency {
 //    public static void countFreq(int arr[], int n)
 //    {
 //        boolean visited[] = new boolean[n];
-//
+
 //        for (int i = 0; i < n; i++) {
-//
+
 //            // Skip this element if already processed
 //            if (visited[i] == true)
 //                continue;
-//
+
 //            // Count frequency
 //            int count = 1;
 //            for (int j = i + 1; j < n; j++) {
@@ -56,4 +57,4 @@ public class countfrequency {
 //            System.out.println(arr[i] + " " + count);
 //        }
 //    }
-//}
+// }
